e09c7617ff771f30e3c93ae72fcd193d807379cc
Jinesh Patel <116644074+jineshpatel7@users.noreply.github.com>
2024-07-21 16:41:32 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -20,7 +20,7 @@ const onSubmitForm = async (e) => {
     try{
     const body={email,password}
 
-    const response = await fetch("http://localhost:5000/auth/login", {
+    const response = await fetch("https://todowithusers.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
