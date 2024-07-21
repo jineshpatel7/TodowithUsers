7997c594a12b7e181f1745c0c21985396f7f59af
Jinesh Patel <116644074+jineshpatel7@users.noreply.github.com>
2024-07-21 16:39:38 +0530

    Update Dashboard.jsx

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -8,7 +8,7 @@ const Dashboard = ({setAuth}) => {
 
   const getName = async ()=>{
     try {
-        const response=await fetch("http://localhost:5000/dashboard/",{
+        const response=await fetch("https://todowithusers.onrender.com/dashboard/",{
           method:"GET",
           headers: {token:localStorage.token}
 
